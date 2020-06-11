32a0b9a7b1a0f7b458c5a9fcc7201842e6589a82
Resha Puspita <reshapuspita734@gmail.com>
2020-06-11 12:14:52 +0700

    delete sentence in line 36

--- buat-kalimat.js
+++ buat-kalimat.js
@@ -33,7 +33,6 @@ console.log(
     "Hai, my name is ", biodata.name,
     ", I studied in ", biodata.college,
     ". My programming skills are ", biodata.skills.programming.language, " including using its framework ", biodata.skills.programming.framework1, ", ", biodata.skills.programming.language2, ", and ", biodata.skills.programming.language3, ". ",
-    "During school, I compete in many sports including  hiking and running.",
     "My hobbies are ", biodata.hobbies.sleeping, " and also ", biodata.hobbies.shopping, ". ",
     "Right now I live at ", biodata.address.street, ", ", biodata.address.province, " - ", biodata.address.country, ".",
     "You can reach me on my email ", biodata.contact.email, " or my phone number ", biodata.contact.phone.countryCode, "-", biodata.contact.phone.number, ".",
